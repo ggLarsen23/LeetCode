0211920f20ab150fbd5cfab2b1419981da97d6ef
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-04-24 22:39:56 +0200

    add Medium Test

---
+++ src/LeetSolutionsMedium.java
@@ -0,0 +1,9 @@
+public class LeetSolutionsMedium {
+
+    public static String longestPalindromeSubstring(String input) {
+        //"ABADA" == "ABA"
+
+        return input;
+    }
+
+}
---
+++ src/LeetSolutionsMediumTest.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LeetSolutionsMediumTest {
+
+    @org.junit.jupiter.api.Test
+    void longestPalindromeSubstringTest() {
+        int[] input = {1,2,3,4,5};
+        LeetSolutionsMedium leetSolutionsEasy = new LeetSolutionsMedium();
+        String output = LeetSolutionsMedium.longestPalindromeSubstring("t");
+
+    }
+}
