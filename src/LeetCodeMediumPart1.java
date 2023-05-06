79d40c2d9292ce938b214726069af44157c0546a
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-05-06 10:36:41 +0200

    Add intToRoman Leetcode

--- src/LeetCodeMediumPart1.java
+++ src/LeetCodeMediumPart1.java
@@ -56,14 +56,78 @@ public class LeetCodeMediumPart1 {
         return characterList.equals(backwardsList);
     }
 
-    public String intToRoman(int num) {
+    public static String intToRoman(int num) {
 
+        String romanString = "";
+        int remainder = num;
         //56 = 50 + 5 + 1
         //
 
-        int newValue = RomanNumerals.L.romanValue;
+        while (remainder > 0)
+        {
+            if (remainder < 0 && num < 4) {
+                remainder -= RomanNumerals.I.romanValue;
+                romanString = romanString.concat(RomanNumerals.I.toString());
+            }
+            else if (remainder == 4) {
+                remainder -= RomanNumerals.IV.romanValue;
+                romanString = romanString.concat(RomanNumerals.IV.toString());
+            }
+            else if (remainder >= 5 && remainder < 9) {
+                remainder -= RomanNumerals.V.romanValue;
+                romanString = romanString.concat(RomanNumerals.V.toString());
+            }
+            else if (remainder == 9) {
+                remainder -= RomanNumerals.IX.romanValue;
+                romanString = romanString.concat(RomanNumerals.IX.toString());
+            }
+            else if (remainder >= 10 && remainder <= 40) {
+                remainder -= RomanNumerals.X.romanValue;
+                romanString = romanString.concat(RomanNumerals.X.toString());
+            }
+            else if (remainder >= 40 && remainder < 50) {
+                remainder -= RomanNumerals.XL.romanValue;
+                romanString = romanString.concat(RomanNumerals.XL.toString());
+            }
+            else if (remainder > 50 && remainder <= 90) {
+                remainder -= RomanNumerals.L.romanValue;
+                romanString = romanString.concat(RomanNumerals.L.toString());
+            }
+            else if (remainder >= 90 && remainder < 100) {
+                remainder -= RomanNumerals.XC.romanValue;
+                romanString = romanString.concat(RomanNumerals.XC.toString());
+            }
+            else if (remainder >= 100 && remainder <= 400) {
+                remainder -= RomanNumerals.C.romanValue;
+                romanString = romanString.concat(RomanNumerals.C.toString());
+            }
+            else if (remainder >= 400 && remainder < 500) {
+                remainder -= RomanNumerals.CD.romanValue;
+                romanString = romanString.concat(RomanNumerals.CD.toString());
+            }
+            else if (remainder > 500 && remainder <= 900) {
+                remainder -= RomanNumerals.D.romanValue;
+                romanString = romanString.concat(RomanNumerals.D.toString());
+            }
+            else if (remainder > 900 && remainder < 1000) {
+                remainder -= RomanNumerals.CM.romanValue;
+                romanString = romanString.concat(RomanNumerals.CM.toString());
+            }
+            else if (remainder >= 1000 && remainder <= 4000) {
+                remainder -= RomanNumerals.M.romanValue;
+                romanString = romanString.concat(RomanNumerals.M.toString());
+            }
+        }
+        return romanString;
+    }
 
-        return null;
+    public static String intToRoman2(int num) {
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] ths ={"","M","MM","MMM"};
+
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 }
 
--- src/LeetCodeMediumPart1Test.java
+++ src/LeetCodeMediumPart1Test.java
@@ -55,4 +55,52 @@ class LeetCodeMediumPart1Test {
         Boolean outputZero = LeetCodeMediumPart1.isPalindrome(inputZero);
         assertEquals(false, outputZero);
     }
+
+    @Test
+    void intToRomanTest() {
+        String romanString = LeetCodeMediumPart1.intToRoman(9);
+        assertEquals("IX", romanString);
+
+        String romanString2 = LeetCodeMediumPart1.intToRoman(10);
+        assertEquals("X", romanString2);
+
+        String romanString3 = LeetCodeMediumPart1.intToRoman(39);
+        assertEquals("XXXIX", romanString3);
+
+        String romanString4 = LeetCodeMediumPart1.intToRoman(45);
+        assertEquals("XLV", romanString4);
+
+        String romanString5 = LeetCodeMediumPart1.intToRoman(125);
+        assertEquals("CXXV", romanString5);
+
+        String romanString6 = LeetCodeMediumPart1.intToRoman(999);
+        assertEquals("CMXCIX", romanString6);
+
+        String romanStrin7 = LeetCodeMediumPart1.intToRoman(1994);
+        assertEquals("MCMXCIV", romanStrin7);
+    }
+
+    @Test
+    void intToRomanTest2() {
+        String romanString = LeetCodeMediumPart1.intToRoman2(9);
+        assertEquals("IX", romanString);
+
+        String romanString2 = LeetCodeMediumPart1.intToRoman2(10);
+        assertEquals("X", romanString2);
+
+        String romanString3 = LeetCodeMediumPart1.intToRoman2(39);
+        assertEquals("XXXIX", romanString3);
+
+        String romanString4 = LeetCodeMediumPart1.intToRoman2(45);
+        assertEquals("XLV", romanString4);
+
+        String romanString5 = LeetCodeMediumPart1.intToRoman2(125);
+        assertEquals("CXXV", romanString5);
+
+        String romanString6 = LeetCodeMediumPart1.intToRoman2(999);
+        assertEquals("CMXCIX", romanString6);
+
+        String romanStrin7 = LeetCodeMediumPart1.intToRoman2(1994);
+        assertEquals("MCMXCIV", romanStrin7);
+    }
 }
