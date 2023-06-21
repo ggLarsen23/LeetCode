8b5f98c19bca088f654dc29e181e22946164adbd
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-06-21 17:43:45 +0200

    Added 2DBinarySearch
    - one wiht O(log(n)*log(m))
    - one with O(log(m*n)

--- src/NeetCode04_BinarySearch.java
+++ src/NeetCode04_BinarySearch.java
@@ -12,14 +12,84 @@ public class NeetCode04_BinarySearch {
             int center = left + (right - left) / 2;
             if (nums[center] == target) {
                 return center;
-            }
-            else if(nums[center] < target) {
+            } else if (nums[center] < target) {
                 left = center + 1;
-            }
-            else if(nums[center] > target) {
+            } else if (nums[center] > target) {
                 right = center - 1;
             }
         }
         return -1;
     }
+
+    /**
+     * 74. Search a 2D Matrix, O(log(n) * log(m)
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int bot = n - 1;
+        int left = 0;
+        int right = m - 1;
+        int correctRow = -1;
+        int correctColumn = -1;
+
+        if (n == 1 && m == 1) {
+            return matrix[0][0] == target;
+        }
+
+        while (top <= bot) {
+            int midX = top + (bot - top) / 2;
+            if (matrix[midX][0] < target && matrix[midX][m - 1] > target) {
+                correctRow = midX;
+                break;
+            } else if (matrix[midX][0] < target) {
+                top = midX + 1;
+            } else if (matrix[midX][0] > target) {
+                bot = midX - 1;
+            }
+        }
+        if (correctRow == -1) return false;
+
+        while (left <= right) {
+            int midY = left + (right - left) / 2;
+            if (matrix[correctRow][midY] == target) {
+                correctColumn = midY;
+                break;
+            } else if (matrix[correctRow][midY] < target) {
+                left = midY + 1;
+            } else if (matrix[correctRow][midY] > target) {
+                right = midY - 1;
+            }
+        }
+        if (correctRow != -1 && correctColumn != -1) {
+            return true;
+        } else return false;
+    }
+
+    /**
+     * 74. Search a 2D Matrix, O(log(n*m)
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int left = 0;
+        int right = n * m - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / m;
+            int col = mid % m;
+
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
 }
--- src/NeetCode04_BinarySearchTest.java
+++ src/NeetCode04_BinarySearchTest.java
@@ -13,4 +13,12 @@ class NeetCode04_BinarySearchTest {
         int output = neetCode04BinarySearch.binarySearch(input, 2);
         assertEquals(-1, output);
     }
+
+    @Test
+    void searchMatrixTest() {
+        int[][] input = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        NeetCode04_BinarySearch neetCode04BinarySearch = new NeetCode04_BinarySearch();
+        boolean output = neetCode04BinarySearch.searchMatrix(input, 3);
+        assertTrue(output);
+    }
 }
