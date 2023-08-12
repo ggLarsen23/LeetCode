04631a583686ed6fcb100989ac84ffaed1007cb5
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-08-12 14:40:44 +0200

    Added Maximum Depth

---
+++ src/NeetCode07_Trees.java
@@ -0,0 +1,16 @@
+import Model.TreeNode;
+
+
+/**
+ * 104. Maximum Depth of Binary Tree
+ */
+public class NeetCode07_Trees {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
