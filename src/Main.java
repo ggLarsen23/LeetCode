58f52d2faf4d0e8cb2b61107e8327e3699a2d3c5
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-09-14 21:40:37 +0200

    Fix ProjectStructure

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         LeetCodeEasyPart1 leetCodeEasyPart1 = new LeetCodeEasyPart1();
 
-        int[] input = {1,2,3,4,5};
+        int[] input = {1, 2, 3, 4, 5};
         int[] output = leetCodeEasyPart1.runningSum(input);
 
-        System.out.println("System Output: " + output.toString());
+        System.out.println("System Output: " + Arrays.toString(output));
     }
 }
